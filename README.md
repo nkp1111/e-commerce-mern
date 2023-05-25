8cfa42744408a1864680011785fd457b1cf04cf9
nkp1111 <neerajp4321@gmail.com>
2023-05-25 21:30:13 +0530

    feat: add product schema, readme and license

---
+++ README.md
@@ -0,0 +1,45 @@
+![License](https://img.shields.io/bower/l/MI)
+
+# E-commerce website
+
+- E-commerce website made using Node.js, Express, React, Redux and MongoDB.
+
+## Table of Contents
+
+- [Overview](#overview)
+- [How to use](#how-to-use)
+- [Contact](#contact)
+
+## Overview
+
+- [GitHub](https://github.com/nkp1111/e-commerce-mern)
+- [Live App]()
+
+- ![screenshot]()
+
+## What i use
+
+- express
+- mongoose
+- dotenv
+
+## How To Use
+
+To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/nkp1111/e-commerce-mern.git
+
+# Install dependencies
+$ npm install
+
+# Run the app
+$ npm start
+```
+
+## Contact
+
+- Neeraj Parmar
+- GitHub [nkp1111](https://github.com/nkp1111)
+- Twitter [@nkp11111507](https://twitter.com/@nkp11111507)
---
+++ server/models/product.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+    maxLength: [100, "Product name cannot be longer than 100 characters"]
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [5, "Product price cannot be longer than 5 characters"],
+    default: 0.0
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  ratings: {
+    type: Number,
+    default: 0
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  category: {
+    type: String,
+    required: [true, "Please enter product category"],
+    enum: {
+      values: ["Electronics", "Furniture", "Clothing", "Books", "Music", "Sports", "Foods", "Beauty/Healthcare",],
+      message: "Please select a valid category"
+    }
+  },
+  seller: {
+    type: String,
+    required: [true, "Please enter product seller"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    maxLength: [5, "Product stock cannot exceed 5 characters"],
+    default: 0
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0
+  },
+  reviews: [{
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+  }]
+}, {
+  timestamps: true
+})
+
+const Product = mongoose.model("Product", productSchema)
+
+module.exports = Product
