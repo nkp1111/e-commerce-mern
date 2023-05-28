5bf7df11487539ae5651fbfcd567805809a9bf68
nkp1111 <neerajp4321@gmail.com>
2023-05-28 13:27:55 +0530

    feat: add errors

--- server/middleware/errors.js
+++ server/middleware/errors.js
@@ -25,6 +25,24 @@ module.exports = (err, req, res, next) => {
       error = new ErrorHandler(message, 400)
     }
 
+    // handling mongoose duplicate key error
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+      error = new ErrorHandler(message, 400)
+    }
+
+    // handling jwt invalid error
+    if (err.name === "JsonWebTokenError") {
+      const message = "JSON web token is invalid. Try again!!!"
+      error = new ErrorHandler(message, 400)
+    }
+
+    // handling jwt expired error
+    if (err.name === "TokenExpiredError") {
+      const message = "JSON web token expired. Try again!!!"
+      error = new ErrorHandler(message, 400)
+    }
+
     res.status(error.statusCode || 500).json({
       success: false,
       message: error.message,
