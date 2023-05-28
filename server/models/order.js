2f86df3624311d8cf7896eb05e68652a280a2188
nkp1111 <neerajp4321@gmail.com>
2023-05-28 18:10:28 +0530

    feat: add order model

---
+++ server/models/order.js
@@ -0,0 +1,108 @@
+const mongoose = require("mongoose")
+
+const orderSchema = new mongoose.Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    zipCode: {
+      type: String,
+      required: true,
+    },
+    phoneNum: {
+      type: String,
+      required: true,
+    },
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      }
+    },
+  ],
+  paymentInfo: {
+    id: {
+      type: String,
+    },
+    status: {
+      type: String,
+    }
+  },
+  paidAt: {
+    type: Date,
+  },
+  itemsPrice: {
+    type: Number,
+    required: true,
+    default: 0.0
+  },
+  shippingPrice: {
+    type: Number,
+    required: true,
+    default: 0.0
+  },
+  taxPrice: {
+    type: Number,
+    required: true,
+    default: 0.0
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0.0
+  },
+  orderStatus: {
+    type: String,
+    required: true,
+    default: "Pending"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  deliveredAt: {
+    type: Date
+  }
+
+})
+
+const Order = mongoose.model("Order", orderSchema)
+
+module.exports = Order
