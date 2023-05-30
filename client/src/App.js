ceb51306e873709b2c9d7988d7908dbdbc87c23d
nkp1111 <neerajp4321@gmail.com>
2023-05-30 10:58:00 +0530

    feat: add client initial files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center'>E-commerce Mern</h1>
+    </div>
+  )
+}
+
+export default App
+
